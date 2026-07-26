2d8f01edcd63576ca22c5a6393c0b0a99cf48242
JANHAVI Kolekar <126249525+janhavi-28@users.noreply.github.com>
2026-07-26 22:11:47 +0530

    LC-intersection-of-two-arrays [Runtime: N/A; Memory: N/A]

---
+++ hash-table/intersection-of-two-arrays.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def intersection(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        ans = []
+
+        for i in range(len(nums1)):
+            for j in range(len(nums2)):
+                if nums1[i] == nums2[j]:
+                    if nums1[i] not in ans:
+                        ans.append(nums1[i])
+
+        return ans
