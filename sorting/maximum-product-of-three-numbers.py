e7c511c8eccc2a13a425dafba0d972872d14574e
JANHAVI Kolekar <126249525+janhavi-28@users.noreply.github.com>
2026-07-26 22:47:44 +0530

    LC-maximum-product-of-three-numbers [Runtime: N/A; Memory: N/A]

--- sorting/maximum-product-of-three-numbers.py
+++ sorting/maximum-product-of-three-numbers.py
@@ -4,11 +4,17 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        maximum = float('-inf')
+        """maximum = float('-inf')
         for i in range(len(nums)):
             for j in range(i+1, len(nums)):
                 for k in range(j+1, len(nums)):  
                     product = nums[i] * nums[j] * nums[k]
                     maximum = max(maximum, product)
-        return maximum
+        return maximum"""
+        nums.sort()
+
+        product1 = nums[-1] * nums[-2] * nums[-3]
+        product2 = nums[0] * nums[1] * nums[-1]
+
+        return max(product1, product2)
         
