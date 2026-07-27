3e0509432ad977c78b4d2129fc22b38610799328
JANHAVI Kolekar <126249525+janhavi-28@users.noreply.github.com>
2026-07-27 21:08:44 +0530

    LC-rotate-array [Runtime: N/A; Memory: N/A]

--- two-pointers/rotate-array.py
+++ two-pointers/rotate-array.py
@@ -1,26 +1,18 @@
 class Solution(object):
-
-    def reverse(self, nums, left, right):
-
-        while left < right:
-            nums[left], nums[right] = nums[right], nums[left]
-
-            left += 1
-            right -= 1
-
-
     def rotate(self, nums, k):
-
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
         n = len(nums)
-
         k = k % n
 
-        # Reverse whole array
-        self.reverse(nums, 0, n - 1)
+        # Reverse the entire array
+        nums.reverse()
 
         # Reverse first k elements
-        self.reverse(nums, 0, k - 1)
+        nums[:k] = reversed(nums[:k])
 
         # Reverse remaining elements
-        self.reverse(nums, k, n - 1)
-        
+        nums[k:] = reversed(nums[k:])
