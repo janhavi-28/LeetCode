e443ea2a9ead2098e244f40e8ba5194eef803555
JANHAVI Kolekar <126249525+janhavi-28@users.noreply.github.com>
2026-07-30 00:18:47 +0530

    LC-valid-anagram [Runtime: 15 ms; Memory: 12.5 MB]

--- hash-table/valid-anagram.py
+++ hash-table/valid-anagram.py
@@ -1,4 +1,29 @@
-class Solution:
-    def isAnagram(self, s: str, t: str) -> bool:
-        from collections import Counter
-        return Counter(s) == Counter(t)
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+
+        if len(s) != len(t):
+            return False
+
+        count = {}
+
+        for ch in s:
+            if ch in count:
+                count[ch] += 1
+            else:
+                count[ch] = 1
+
+        for ch in t:
+            if ch not in count:
+                return False
+
+            count[ch] -= 1
+
+            if count[ch] < 0:
+                return False
+
+        return True
