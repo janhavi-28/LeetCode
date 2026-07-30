803ec55b2eb3e5d8bd30d91355c7eec810c18ad9
JANHAVI Kolekar <126249525+janhavi-28@users.noreply.github.com>
2026-07-30 22:33:47 +0530

    LC-valid-palindrome [Runtime: 146 ms; Memory: 13.1 MB]

--- string/valid-palindrome.py
+++ string/valid-palindrome.py
@@ -1,9 +1,25 @@
 class Solution(object):
     def isPalindrome(self, s):
-        new = ""
-        for i in s:
-            if i.isalnum():
-                new += i.lower()
-        return new == new[::-1]
+        """
+        :type s: str
+        :rtype: bool
+        """
 
-        
+        clean = ""
+
+        for ch in s:
+            if ch.isalnum():
+                clean += ch.lower()
+
+        left = 0
+        right = len(clean) - 1
+
+        while left < right:
+
+            if clean[left] != clean[right]:
+                return False
+
+            left += 1
+            right -= 1
+
+        return True  
