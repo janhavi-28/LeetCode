8240010533ac635c761624b2b6d0b24f60961eea
JANHAVI Kolekar <126249525+janhavi-28@users.noreply.github.com>
2026-07-31 00:53:28 +0530

    LC-first-unique-character-in-a-string [Runtime: N/A; Memory: N/A]

--- string/first-unique-character-in-a-string.py
+++ string/first-unique-character-in-a-string.py
@@ -1,9 +1,18 @@
-class Solution:
-    def firstUniqChar(self, s: str) -> int:
-        from collections import Counter
-        freq = Counter(s)
-        
-        for i, ch in enumerate(s):
-            if freq[ch] == 1:
+class Solution(object):
+    def firstUniqChar(self, s):
+
+        count = {}
+
+        # Count frequency of each character
+        for ch in s:
+            if ch in count:
+                count[ch] += 1
+            else:
+                count[ch] = 1
+
+        # Find first unique character
+        for i in range(len(s)):
+            if count[s[i]] == 1:
                 return i
+
         return -1
