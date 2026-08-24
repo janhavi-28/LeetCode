a585785f3462bbd7a4fda7bb147cc5fc0edbab4f
JANHAVI Kolekar <126249525+janhavi-28@users.noreply.github.com>
2026-08-24 22:12:13 +0530

    LC-longest-substring-without-repeating-characters [Runtime: N/A; Memory: N/A]

--- hash-table/longest-substring-without-repeating-characters.py
+++ hash-table/longest-substring-without-repeating-characters.py
@@ -4,18 +4,27 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        low = 0
-        res = 0
-        freq = {}
+
+        low = 0          # Left pointer
+        res = 0          # Maximum length
+        freq = {}        # Store frequency of characters
+
+        # Move right pointer
         for high in range(len(s)):
-        #include current character
+
+            # Include current character
             freq[s[high]] = freq.get(s[high], 0) + 1
-        #shirnk window if duplicate found
+
+            # If duplicate exists, shrink window
             while freq[s[high]] > 1:
                 freq[s[low]] -= 1
+
                 if freq[s[low]] == 0:
                     del freq[s[low]]
+
                 low += 1
-            #Update maximum length
-                res = max(res, high - low + 1)
-        return res        
+
+            # Update answer
+            res = max(res, high - low + 1)
+
+        return res
