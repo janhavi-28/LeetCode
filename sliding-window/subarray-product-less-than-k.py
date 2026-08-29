11b7deaae433aeda081b1f5651c6696aab4db33c
JANHAVI Kolekar <126249525+janhavi-28@users.noreply.github.com>
2026-08-29 23:55:08 +0530

    LC-subarray-product-less-than-k [Runtime: N/A; Memory: N/A]

--- sliding-window/subarray-product-less-than-k.py
+++ sliding-window/subarray-product-less-than-k.py
@@ -1,5 +1,8 @@
 class Solution:
     def numSubarrayProductLessThanK(self, nums: List[int], k: int) -> int:
+
+        if k <= 1:
+            return 0
         low = 0
         res = 0
         product = 1
