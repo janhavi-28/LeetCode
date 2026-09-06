c7981060c01d99b64a5cf8cb70013920ba6c76d1
JANHAVI Kolekar <126249525+janhavi-28@users.noreply.github.com>
2026-09-06 20:58:50 +0530

    LC-happy-number [Runtime: N/A; Memory: N/A]

---
+++ two-pointers/happy-number.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        def fun(n):
+            total = 0
+            while n > 0:
+                d = n % 10
+                n = n // 10
+                total += d * d
+            return total
+        slow = n
+        fast = n
+        while fast != 1:
+            slow = fun(slow)
+            fast = fun(fun(fast))
+            if (slow == fast and slow != 1):
+                return False
+        Fast = 1
+        return True
